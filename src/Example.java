b14091c46c7e1e92443cad48382345a85a1728fd
MattBrown <litteriver1337@gmail.com>
2015-10-22 18:39:05 -0400

    The start of my work-book exercises!

---
+++ src/Example.java
@@ -0,0 +1,18 @@
+/**
+ * Created by MattBrown on 10/21/15.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Java drives the Web.");
+
+        int var1;
+        int var2;
+
+        var1 = 1024;
+        var2 = var1 / 2;
+
+        System.out.println("var1 conatins" + var1);
+        System.out.println("var2 containts var1 / 2: ");
+        System.out.println(var2);
+    }
+}
